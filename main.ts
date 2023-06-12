44ffc7e6536e0012bacefbdb33c4b322136cda4e
Bunker D <contact@bunkerd.fr>
2023-06-11 19:49:14 -0700

    Collected mermaid definitions

--- main.ts
+++ main.ts
@@ -1,15 +1,14 @@
 import { Plugin, addIcon, Notice } from 'obsidian';
-// import { App, Editor, MarkdownView, Modal, Notice, PluginSettingTab, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+//TODO For test purposes, make buttons for each chart type, that modifies its contained IDs for activate/deactivate, to check the effects of each
 
-// interface MyPluginSettings {
-// 	mySetting: string;
-// }
+//TODO A function to get the Mermaid definitions based on selected chart types
+//TODO   → Must put those definitions in a <defs>
+//TODO   → Must communicate incompatible definition overlap
 
-// const DEFAULT_SETTINGS: MyPluginSettings = {
-// 	mySetting: 'default'
-// };
+//TODO Setting page for selecting the covered chart types
+
+//TODO Alternative option: select the chart types dynamically based on active tab?
 
 const EMPTY_ICON = true;
 const ARROW_ICON = `
@@ -19,9 +18,7 @@ const ARROW_ICON = `
 		</g>
 	`;
 
-export default class MyPlugin extends Plugin {
-	// settings: MyPluginSettings;
-
+export default class MermaidArrowSaver extends Plugin {
 	async onload() {
 		const mermaidMarkers = `
 				<g fill="currentColor" stroke="currentColor">
@@ -58,79 +55,9 @@ export default class MyPlugin extends Plugin {
 		this.addRibbonIcon( iconName, 'Mermaid markers', ( evt: MouseEvent ) => {
 			new Notice( 'This button keeps Mermaid arrows visible.\nClicking it does nothing.' );
 		} );
-
-		// // TODO ▼ ?
-		// await this.loadSettings();
-
-		// // This creates an icon in the left ribbon.
-		// // Perform additional things with the ribbon
-		// ribbonIconEl.addClass( 'my-plugin-ribbon-class' );
-
-		// // This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		// const statusBarItemEl = this.addStatusBarItem();
-		// statusBarItemEl.setText( 'Status Bar Text' );
-
-		// // This adds a simple command that can be triggered anywhere
-		// this.addCommand( {
-		// 	id: 'open-sample-modal-simple',
-		// 	name: 'Open sample modal (simple)',
-		// 	callback: () => {
-		// 		new SampleModal( this.app ).open();
-		// 	}
-		// } );
-		// // This adds an editor command that can perform some operation on the current editor instance
-		// this.addCommand( {
-		// 	id: 'sample-editor-command',
-		// 	name: 'Sample editor command',
-		// 	editorCallback: ( editor: Editor, view: MarkdownView ) => {
-		// 		console.log( editor.getSelection() );
-		// 		editor.replaceSelection( 'Sample Editor Command' );
-		// 	}
-		// } );
-		// // This adds a complex command that can check whether the current state of the app allows execution of the command
-		// this.addCommand( {
-		// 	id: 'open-sample-modal-complex',
-		// 	name: 'Open sample modal (complex)',
-		// 	checkCallback: ( checking: boolean ) => {
-		// 		// Conditions to check
-		// 		const markdownView = this.app.workspace.getActiveViewOfType( MarkdownView );
-		// 		if ( markdownView ) {
-		// 			// If checking is true, we're simply "checking" if the command can be run.
-		// 			// If checking is false, then we want to actually perform the operation.
-		// 			if ( !checking ) {
-		// 				new SampleModal( this.app ).open();
-		// 			}
-
-		// 			// This command will only show up in Command Palette when the check function returns true
-		// 			return true;
-		// 		}
-		// 	}
-		// } );
-
-		// // This adds a settings tab so the user can configure various aspects of the plugin
-		// this.addSettingTab( new SampleSettingTab( this.app, this ) );
-
-		// // If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// // Using this function will automatically remove the event listener when this plugin is disabled.
-		// this.registerDomEvent( document, 'click', ( evt: MouseEvent ) => {
-		// 	console.log( 'click', evt );
-		// } );
-
-		// // When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		// this.registerInterval( window.setInterval( () => console.log( 'setInterval' ), 5 * 60 * 1000 ) );
 	}
 
-	onunload() {
-
-	}
-
-	// async loadSettings() {
-	// 	this.settings = Object.assign( {}, DEFAULT_SETTINGS, await this.loadData() );
-	// }
-
-	// async saveSettings() {
-	// 	await this.saveData( this.settings );
-	// }
+	onunload() {}
 }
 
 // class SampleModal extends Modal {
---
+++ types.ts
@@ -0,0 +1,3 @@
+export type ChartType = string;
+export type ID = string;
+export type SVGContent = string;
